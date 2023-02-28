84f30ad450d8251399fa73d68164d5a132d3dca8
eduado <eduardo.hor@outlook.com>
2023-02-28 17:30:05 -0300

    feat: create crud users

--- index.php
+++ index.php
@@ -49,6 +49,91 @@ $app->get('/admin/logout', function(){
 	exit;
 });
 
-$app->run();
+$app->get('/admin/users', function(){
+	User::verifyLogin();
 
- ?>
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl('users', [
+		'users' => $users
+	]);
+
+
+});
+
+$app->get('/admin/users/create', function(){
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl('users-create');
+
+
+});
+
+$app->get('/admin/users/:iduser/delete', function($idUser){
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$idUser);
+
+	$user->delete();
+
+	header('Location: /admin/users');
+	exit;
+	
+});
+
+$app->get('/admin/users/:iduser', function($idUser){
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$idUser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl('users-update', [
+		'user' => $user->getValues()
+	]);
+
+});
+
+$app->post('/admin/users/create', function(){
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST['indadmin'] = (isset($_POST['inadmin']))?1:0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->post('/admin/users/:iduser', function($idUser){
+	User::verifyLogin();
+	
+	$user = new User();
+
+	$user->get((int)$idUser);
+
+	$_POST['indadmin'] = (isset($_POST['inadmin']))?1:0;
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->run();
